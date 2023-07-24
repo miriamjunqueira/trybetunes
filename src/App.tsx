9178fdb8623ffa389e48a7e699a31bf7762d5e1d
Miriam <miriam.junqueira@outlook.com>
2023-07-24 15:06:08 -0300

    Inclusao do requisito 2.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <p>Trybetunes</p>
       <Routes>
         <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
       </Routes>
     </>
   );
---
+++ src/pages/Search.tsx
@@ -0,0 +1,87 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+
+function Search() {
+  const [nomeArtista, setNomeArtista] = useState('');
+  const [nomeArtistaBuscado, setNomeArtistaBuscado] = useState('');
+  const [listaArtistas, setListaArtistas] = useState<any[]>([]);
+
+  function valida() {
+    return (nomeArtista.length >= 2);
+  }
+
+  function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+    setNomeArtista(event.target.value);
+  }
+
+  function resetForm() {
+    setNomeArtista('');
+  }
+
+  async function handleSubmit(event: FormEvent<HTMLElement>) {
+    event.preventDefault();
+    if (valida()) {
+      setListaArtistas(await searchAlbumsAPI(nomeArtista));
+      setNomeArtistaBuscado(nomeArtista);
+      resetForm();
+    }
+  }
+
+  return (
+    <div>
+      <form
+        className="form-de-busca"
+        onSubmit={ handleSubmit }
+      >
+        <label htmlFor="search-input">Digite o nome do artista que procura:</label>
+        <input
+          type="text"
+          name="nomeArtista"
+          id="search-input"
+          required
+          onChange={ handleSearchChange }
+          value={ nomeArtista }
+          data-testid="search-artist-input"
+        />
+
+        <button
+          type="submit"
+          disabled={ (!valida()) }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
+      </form>
+
+      {(listaArtistas.length === 0)
+
+        ? (
+          <div>
+            <p>Nenhum álbum foi encontrado</p>
+          </div>
+        )
+        : (
+          <div>
+            <p>{`Resultado de álbuns de: ${nomeArtistaBuscado}`}</p>
+            {listaArtistas.map((artista) => {
+              return (
+                <div key={ artista.collectionId }>
+                  <a
+                    href={ `/album/${artista.collectionId}` }
+                    data-testid={ `link-to-album-${artista.collectionId}` }
+                  >
+                    link
+
+                  </a>
+                  <p>{artista.collectionName}</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+
+    </div>
+  );
+}
+
+export default Search;
