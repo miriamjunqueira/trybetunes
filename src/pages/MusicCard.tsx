77cba52272571f0ee5f29cdea8071859a491d8a1
Miriam <miriam.junqueira@outlook.com>
2023-08-07 14:07:06 -0300

    Revert "ajustes linter"
    This reverts commit 52f40f99572661731e7ac6545262f14c8ac317de.

--- src/pages/MusicCard.tsx
+++ src/pages/MusicCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { SongType } from '../types';
 
 type MusicCardType = {
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Album from './Album';
 
 function Search() {
   const [nomeArtista, setNomeArtista] = useState('');
