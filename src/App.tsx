73a8cd8a90608a28a2a526bccfda8302da9de76a
Miriam <miriam.junqueira@outlook.com>
2023-08-07 14:08:21 -0300

    Requisito 4.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <>
       <p>Trybetunes</p>
       <Routes>
-        <Route path="/" element={ <Login /> } />
-        <Route path="/search" element={ <Search /> } />
-        <Route path="/album/:id" element={ <Album /> } />
+        <Route path="/" element={ <Layout /> }>
+          <Route path="/search" element={ <Search /> } />
+          <Route path="/album/:id" element={ <Album /> } />
+        </Route>
+        <Route index element={ <Login /> } />
       </Routes>
     </>
   );
---
+++ src/pages/Header.tsx
@@ -0,0 +1,50 @@
+import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Loading from '../services/Loading';
+import { getUser } from '../services/userAPI';
+
+function Header() {
+  const [carregando, setCarregando] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
+
+  useEffect(() => {
+    async function recuperaInfosUsuario() {
+      const infos = await getUser();
+      setNomeUsuario(infos.name);
+      setCarregando(false);
+    }
+    recuperaInfosUsuario();
+  }, []);
+
+  return (
+    <header data-testid="header-component">
+      {carregando
+        ? (
+          <div>
+            <Loading />
+          </div>
+        )
+        : (
+          <div>
+            <p data-testid="header-user-name">
+              Bem vindo
+              {' '}
+              {`${nomeUsuario}`}
+            </p>
+
+            <nav id="nav">
+              <NavLink data-testid="link-to-search" to="/search">Search</NavLink>
+              {' '}
+              <NavLink data-testid="link-to-favorites" to="/favourites">
+                Favourites
+              </NavLink>
+              {' '}
+              <NavLink data-testid="link-to-profile" to="/profile">Profile</NavLink>
+            </nav>
+          </div>
+        )}
+    </header>
+  );
+}
+
+export default Header;
